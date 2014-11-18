13fea15b202ea2580c750163a0ff6a5da35dddae
mpearce25 <pearcematthew26@gmail.com>
2014-11-18 17:48:27 -0500

    fdgdfgfghdfhy

--- src/text_lab_04/text_lab_04_matthew_pearce.java
+++ src/text_lab_04/text_lab_04_matthew_pearce.java
@@ -16,7 +16,7 @@ public class text_lab_04_matthew_pearce
 			System.out.println();
 			System.out.println("Entered String:     " + str);
 			System.out.println("Palindrome:         " + Palindrome.isPal(str));
-			System.out.println("Almost Palindrome:  " + Palindrome.almostPal(str));	  // used only for the 100 point version
+			System.out.println("Almost Palindrome:  " + Palindrome.almostPal(str));	  
 			System.out.print("Do you wish to repeat this program [Y/N]?  ===>>  ");
 			String repeat = input.nextLine();
 			notFinished = (repeat.equals("Y")) || (repeat.equals("y"));
