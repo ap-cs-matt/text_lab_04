acb7299018114d8f2bfb31c5f252b4bc772bba39
mpearce25 <pearcematthew26@gmail.com>
2014-11-18 22:19:11 -0500

    commented out unnesary

--- src/text_lab_04/text_lab_04_matthew_pearce.java
+++ src/text_lab_04/text_lab_04_matthew_pearce.java
@@ -44,12 +44,12 @@ class Palindrome
     	 if (i == length)
     	 {
     		 reverse = reverse + s.substring(m);
-    		 System.out.println(reverse);
+    		 //System.out.println(reverse);
     	 }
     	 else
     	 {
     		 reverse = reverse + s.substring(m-1,m); 
-    		 System.out.println(reverse);
+    		 //System.out.println(reverse);
     	 }
     	 
     	 m--;
